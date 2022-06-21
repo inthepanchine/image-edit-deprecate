d5d1dc04c60d9435e1f033c75391093178c6f587
Liam Caccetta <liam.caccetta@icloud.com>
2022-06-21 12:53:28 +0200

    Generate doc for v0.0.2

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@
 export { Editor } from "./classes/editor";
 export { FileHandler } from "./classes/file";
 export { Selection } from "./classes/selection";
+export { ImageStyle, CanvasImage } from "./types/editorTypes";
+export { SelectedRegion } from "./types/selectionTypes";
+export { Size, Position } from "./types/globalTypes";
