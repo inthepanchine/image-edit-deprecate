99b4a8eee467f49108eb4d5f00de337ea12f429e
Liam Caccetta <liam.caccetta@icloud.com>
2022-06-21 12:44:26 +0200

    Updated package.json + Added linting
    - Update package's version
    - Update all dependencies to latest version
    - Add lint script with eslint
    - Install new needed eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	root: true,
 	extends: "@react-native-community",
-	parser: "babel-eslint",
+	parser: "@babel/eslint-parser",
 	plugins: ["@typescript-eslint"],
 	extends: ["prettier"],
 };
