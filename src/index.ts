30a6ddc9e4e403c476249309b4a1ba86776e1588
Liam Caccetta <liam.caccetta@icloud.com>
2022-06-21 14:26:07 +0200

    Redo documentation due to error in export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,3 @@
 export { Editor } from "./classes/editor";
 export { FileHandler } from "./classes/file";
 export { Selection } from "./classes/selection";
-export { ImageStyle, CanvasImage } from "./types/editorTypes";
-export { SelectedRegion } from "./types/selectionTypes";
-export { Size, Position } from "./types/globalTypes";
